cd6323dec81dca39e1ca2dabb409bc0ad76af2a4
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-10 14:40:34 -0600

    Fixed event delegation bug that prevented item deletion after a new item was added.

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-const deleteButtons = document.querySelectorAll('.delete-button');
-const addItemButton = document.querySelector('#addItemButton');
+let toDoList = document.querySelector('.todo-list');
+let addItemButton = document.querySelector('#addItemButton');
 
-deleteButtons.forEach(button => {
-  button.addEventListener('click', function() {
-    const itemId = this.parentElement.getAttribute('data-item-id');
-    const itemToDelete = document.querySelector(`[data-item-id="${itemId}"]`);
-    
+
+toDoList.addEventListener('click', function(event) {
+  if (event.target.classList.contains('delete-button')) {
+    let itemToDelete = event.target.parentElement;
     if (itemToDelete) {
       itemToDelete.remove();
     }
-  });
+  }
 });
 
 addItemButton.addEventListener('click', () => {
-    const toDoList = document.querySelector('.todo-list');
-    const toDoInput = document.querySelector('#toDoInput').value;
-
-
-    console.log(toDoInput);
-    toDoList.innerHTML += `<li>${toDoInput}</li>`;
-})
+  let toDoInput = document.querySelector('#toDoInput').value;
+  toDoList.innerHTML += `<li>${toDoInput} <button class="delete-button">Done</button></li>`;
+});
